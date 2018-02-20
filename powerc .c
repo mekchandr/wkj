51686cf2ba4af3bc638523ccb0d4a04439ed0622
mekchandr <35653274+mekchandr@users.noreply.github.com>
2018-02-20 16:28:04 +0530

    Rename power .c to powerc .c

---
+++ powerc .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main ()
+{
+    int a,b,c,i=1;
+    printf ("enter a and b");
+    scanf("%d%d",&a,&b);
+    for (c=1;c<=b;c++)
+    i=i*a;
+    {
+        printf ("power %d",i);
+    }
+    
+}
+
